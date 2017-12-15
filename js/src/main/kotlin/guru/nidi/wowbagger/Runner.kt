c14fbbbc6104bd7e2df54827036bc21552946103
Stefan Niederhauser <ghuder5@gmx.ch>
2017-12-15 11:53:33 +0100

    add html demo

--- js/src/main/kotlin/guru/nidi/wowbagger/Runner.kt
+++ js/src/main/kotlin/guru/nidi/wowbagger/Runner.kt
@@ -5,14 +5,13 @@ import guru.nidi.wowbagger.Wowbagger.adjective
 import guru.nidi.wowbagger.Wowbagger.interjection
 import guru.nidi.wowbagger.Wowbagger.name
 import guru.nidi.wowbagger.Wowbagger.subject
-import kotlin.browser.document
 
-fun main(args: Array<String>) {
-   val int = interjection().capitalize()
-   val name = name()
-   val subject = subject(name.gender)
-   val adj1 = adjective(subject.gender)
-   val adj2 = adjective(subject.gender)
-   val action = action()
-   document.getElementById("text")!!.textContent="$int $name, du $adj1 $adj2 $subject, $action"
+fun insult(): String {
+    val int = interjection().capitalize()
+    val name = name()
+    val subject = subject(name.gender)
+    val adj1 = adjective(subject.gender)
+    val adj2 = adjective(subject.gender)
+    val action = action()
+    return "$int $name, du $adj1 $adj2 $subject, $action"
 }
