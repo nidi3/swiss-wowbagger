0804f37dbeb04c31ccdcb9df0821af6a0be6be5a
Stefan Niederhauser <ghuder5@gmx.ch>
2017-12-16 01:56:14 +0100

    add share buttons

--- common/src/main/kotlin/guru/nidi/wowbagger/Actions.kt
+++ common/src/main/kotlin/guru/nidi/wowbagger/Actions.kt
@@ -4,122 +4,122 @@ import guru.nidi.wowbagger.Wowbagger.trimLines
 
 object Actions {
     val list = """
-geit's eigentlech no?
-bisch nid ganz bache?
-hesch en Egge ab?
 aber süsch bisch no gsung?
-hesch es warms Joghurt im Sack?
-het's dr i Weize gschneit?
-bisch bisse?
-hesch e Flick furt?
-het's dr is Hirni geschisse?
-bisch nid ganz hundert?
-hesch Mähl am Ermu?
 aber süsch geit's?
-verreis!
-haut der Latz!
-fahr ab!
-schieb ne!
-hou's!
-putz di furt!
-häb doch mau d'Schnure!
-schlift's?
-haut s'Gfräs!
-du hesch so ne längi Leitig!
-hesch z'heiss badet?
-hesch e Knick ir Fichte?
-i zeig dr wo de Bartli de Most hout!
 adiemerssi!
-gang doch go bääbele!
-hesch z'vüu Bätziwasser gha?
-i brätsche der eis!
 bi dir chauberet doch der Brüggstock!
-hesch ir Bschütti badet?
-tue nid chafle!
+bisch bisse?
+bisch nid ganz bache?
+bisch nid ganz hundert?
+bisch ir Gäggelischuel blibe hocke?
+bisch mit em Gertu usgrütscht?
+bisch ir Spinnhumbbele blibe stecke?
+bisch am Haag a?
 chunsch de is Chefi!
-wosch e Chlapf a Grind?
-was chnüblisch do?
+du hesch so ne längi Leitig!
+fahr ab!
+gang doch go bääbele!
+geit's eigentlech no?
+haut der Latz!
+haut s'Gfräs!
+hesch e Flick furt?
+hesch e Knick ir Fichte?
+hesch en Egge ab?
+hesch es warms Joghurt im Sack?
+hesch Mähl am Ermu?
+hesch z'heiss badet?
+hesch z'vüu Bätziwasser gha?
+hesch ir Bschütti badet?
+hesch de Gluggsi?
+hesch e dräckige Naselumpe!
+heschs Gschtäutli verlore?
+hesch nüm aui Lätsche ar Lismete?
+hesch e stumpfe Hegu?
+hesch nid aui Scheieli am Hag?
+hesch z'vüu ploderet?
+heschs Ohremüggeli?
+hesch Gsüchti?
+hesch es Gnusch im Fadechörbli?
+het's dr i Weize gschneit?
+het's dr is Hirni geschisse?
+hou's!
+häb doch mau d'Schnure!
+hör uf gfätterle!
+hör uf lauere!
+hör uf gigampfe!
+hör uf zaagge!
+hör uf liire!
+i brätsche der eis!
+i gheie di i Ghüder!
+i gib der mit em Teppichchlopfer!
+i gorbse der i Gring!
+i müpfe di i See!
 i putz der d'Chuttle!
+i tätsche der eis!
+i gib der mit der Wedele!
+i nimm di am Grännihaar!
+i poliere der s'Gröibschi
+i tue der ds Gurli fiegge!
+i säg der au Gott Schand!
+i verminggle dir s'Bätzi dass d'Osterpföteler ghörsch zawanggle!
+i zeih der d'Gängeretalpli läng!
+i vermöckle der de Schtirps mit em Flarzise!
+i zwurgle di mit dem Gürchu!
+i bring di auf d'Gnagihoschtet!
+i treichle der öpis!
+i gib der eis uf'd Rösti!
+i zeig dr wo de Bartli de Most hout!
+putz di furt!
+schieb ne!
+schlift's?
+tue nid töipele!
+tue mou richtig wärche!
+tue mer am Gätzi rätsche!
+tuesch hurti öpis härejufle?
+tue nid mit em Gagu spile!
+tue z'Nüni näh!
+tue nid chafle!
+verreis!
+wosch e Chlapf a Grind?
+was isch mit dim Hosegschingg?
+was hesch gänggelet?
+was schilisch?
+was fuschtisch do?
+was machsch für ne Lätsch?
+wärsch froh hättisch en einzige nuesige Schiggeler uf em Lugipfupf!
+wird der gschmuech?
+was chnüblisch do?
 mues me di dängele?
 gang dänne!
 dir muess me s'Füdle fäge!
-was fuschtisch do?
 geisch mer ufs Gäder!
-hör uf gfätterle!
-i gheie di i Ghüder!
-hör uf lauere!
-i gib der mit em Teppichchlopfer!
-hör uf liire!
-hesch de Gluggsi?
 gump doch ine Glungge!
-hesch es Gnusch im Fadechörbli?
-i gorbse der i Gring!
 muess me di früsch griene?
-wird der gschmuech?
-hesch Gsüchti?
-hör uf gigampfe!
 hantli is Bett!
-tue z'Nüni näh!
 gang doch ar Aare nah!
-tue nid mit em Gagu spile!
-was machsch für ne Lätsch?
 gsesch us wie nes Meertrübeli!
 du meggisch!
-i müpfe di i See!
 schnütz di mou!
-hesch e dräckige Naselumpe!
-heschs Ohremüggeli?
 bääg doch nid so blöd!
-hesch z'vüu ploderet?
 jetz räblet's!
-i gib der eis uf'd Rösti!
 Houptsach es rouchnet, Housi!
-hesch nid aui Scheieli am Hag?
-was schilisch?
 du bisch zum schiesse!
 hör uf spöifere!
 häb d'Talpe wäg!
-i tätsche der eis!
-tue nid töipele!
-tue mou richtig wärche!
-i gib der mit der Wedele!
-hör uf zaagge!
-i nimm di am Grännihaar!
 gang go z'Acher fahre!
-tue mer am Gätzi rätsche!
-bisch am Haag a?
 rum di Göppu dänne!
-i treichle der öpis!
-was hesch gänggelet?
-heschs Gschtäutli verlore?
-i poliere der s'Gröibschi
-bisch ir Gäggelischuel blibe hocke?
-bisch mit em Gertu usgrütscht?
-i bring di auf d'Gnagihoschtet!
-hesch e stumpfe Hegu?
-tuesch hurti öpis härejufle?
-hesch nüm aui Lätsche ar Lismete?
-i tue der ds Gurli fiegge!
-bisch ir Spinnhumbbele blibe stecke?
-i säg der au Gott Schand!
-i verminggle dir s'Bätzi dass d'Osterpföteler ghörsch zawanggle!
-wärsch froh hättisch en einzige nuesige Schiggeler uf em Lugipfupf!
-schnäggelisch grad ziemlech us dr Gosche!
-schmöcksch wie bschüttigs Chrützimähl!
-chunsch grad usem Chätschäbertrog?
-was isch mit dim Hosegschingg?
-i zeih der d'Gängeretalpli läng!
-i zwurgle di mit dem Gürchu!
-guuggets der im Schissächerli?
-i vermöckle der de Schtirps mit em Flarzise!
-du chasch nid mou s'Gräppli im Hotschmägli bleike!
-grunz nid so grusig!
-schnorsch grusig wiene Moore!
 bohr nid i de Ohre!
-schläck nid a de Böögge!
 chotz nid so!
+chunsch grad usem Chätschäbertrog?
+chunnt de Wurschtsalat zrügg?
+du chasch nid mou s'Gräppli im Hotschmägli bleike!
 furz nid so i der Chile!
 friss nid us der Chatzechischte!
-chunnt de Wurschtsalat zrügg?
+grunz nid so grusig!
+guuggets der im Schissächerli?
+schnäggelisch grad ziemlech us dr Gosche!
+schmöcksch wie bschüttigs Chrützimähl!
+schnorsch grusig wiene Moore!
+schläck nid a de Böögge!
 """.trimLines()
 }
